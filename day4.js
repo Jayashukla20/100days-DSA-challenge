d87f2d2b814db6bb12143a1edd5a358dcd8ff943
jayashukla20 <jayashukla1308@gmail.com>
2025-09-10 22:46:34 +0530

    Resolved merge conflict in day4.js

--- day4.js
+++ day4.js
@@ -1,12 +1,7 @@
-<<<<<<< HEAD
+
 let invoices = [1001, 1002, 1003, 1004];
 let INV = invoices.map(x => "INV-" + x)
 console.log(INV)
 // ✅ Expected Output:
-=======
-let invoices = [1001, 1002, 1003, 1004];
-let INV = invoices.map(x => "INV-" + x)
-console.log(INV)
-// ✅ Expected Output:
->>>>>>> 8f41fdd (adding file)
+
 // ['INV-1001', 'INV-1002', 'INV-1003', 'INV-1004']
